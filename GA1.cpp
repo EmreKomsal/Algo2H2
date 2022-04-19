0ee8adac7c13d4b6911977c4295cc0e76fa96c5d
EmreKomsal <komsal16@itu.edu.tr>
2022-04-19 17:38:55 +0300

    GA1 classını yazdım

--- GA1.cpp
+++ GA1.cpp
@@ -5,6 +5,10 @@ using namespace std;
 class Color{
 public:
     int color_id;
+
+    Color(int id){
+        color_id = id;
+    }
 };
 class Node{
 private:
@@ -50,9 +54,20 @@ int main() {
     Node node3(3);
     Node node4(4);
 
+    Color c1(1);
+    Color c2(2);
+    Color c3(3);
+    Color c4(4);
+    Color c5(5);
+
     node1.Set_Adjacent(&node2);
     node1.Set_Adjacent(&node3);
     node1.Set_Adjacent(&node4);
+
+    node1.Set_Colour(&c1);
+    node2.Set_Colour(&c2);
+    node3.Set_Colour(&c3);
+    node4.Set_Colour(&c4);
 
     node1.Print_Adjacent();
 }
